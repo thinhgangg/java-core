dba050ee7be0f7acde74823a204f9c056c401293
Phuc Thinh <phucthinhmai00@gmail.com>
2025-01-22 21:38:41 +0700

    chapter 11

---
+++ src/chapter11/test.java
@@ -0,0 +1,21 @@
+package chapter11;
+
+public class test extends Thread {
+	@Override
+	public void run() {
+		System.out.println("start thread");
+		long sum = 0L;
+		for (long i = 0L; i < 10000000000L; i++) {
+			sum += i;
+		}
+		System.out.println("end thread" + sum);
+	}
+
+	public static void main(String[] args) {
+		int max = 8;
+		for (int i = 0; i < max; i++) {
+			(new test()).start();
+		}
+		System.out.println("finish");
+	}
+}
